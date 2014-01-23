15e146366e1214c7658b2f9019e3e4974014441f
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 12:21:20 -0200

    Update applied to Livro.java

--- src/app/models/Livro.java
+++ src/app/models/Livro.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)Autor.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package app.models;
 
 import java.util.List;
@@ -13,128 +30,89 @@ import jedi.db.models.QuerySet;
 @SuppressWarnings({"rawtypes", "unchecked"})
 @Table(name="livros", engine="InnoDB", charset="utf8", comment="Tabela que armazena os livros do sistema de biblioteca.")
 public class Livro extends Model {
-//	Atributos
-	private static final long serialVersionUID = 9076408430303339094L;
-	
-	@CharField(max_length=30, required=true, unique=true, comment="Esse campo armazena o nome do livro.")
-	private String titulo;
-	
-	@ManyToManyField(model="Autor", references="autores")
-	private QuerySet<Autor> autores;
-	
-	@ForeignKeyField(model="Editora", constraint_name="fk_livros_editoras", references="editoras")
-	private Editora editora;
-	
-	@CharField(max_length=15, required=true, comment="Data de publicação do livro.")
-	private String dataPublicacao;
-	
-	// Registrando o model manager para a classe de objetos livro.
-	public static Manager objects = new Manager(Livro.class);
-	
-//	public static Manager objects = new Manager<Livro>();
-	
-//	Construtores
-	public Livro() {}
-	
-	public Livro(int id) {
-		this.id = id;
-	}
-	
-	public Livro(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public Livro(String titulo, List<Autor> autores, Model editora, String dataPublicacao) {
-		this.titulo = titulo;
-		this.autores = new QuerySet<Autor>();
-		this.autores.add(autores);
-		this.editora = (Editora) editora;
-		this.dataPublicacao = dataPublicacao;
-	}
-	
-	public Livro(int id, String titulo, List<Autor> autores, Model editora, String dataPublicacao) {
-		this.id = id;
-		this.titulo = titulo;
-		this.autores = new QuerySet<Autor>();
-		this.autores.add(autores);
-		this.editora = (Editora) editora;
-		this.dataPublicacao = dataPublicacao;
-	}
-	
-//	Getters
-	public String getTitulo() {
-		return titulo;
-	}
+    // Attributes
+    private static final long serialVersionUID = 9076408430303339094L;
 
-	public String titulo() {
-		return titulo;
-	}
-	
-	public QuerySet getAutores() {
-		return autores;
-	}
-	
-	public QuerySet autores() {
-		return autores;
-	}
-	
-	public Editora getEditora() {
-		return editora;
-	}
+    @CharField(max_length=30, required=true, unique=true, comment="Esse campo armazena o nome do livro.")
+    private String titulo;
 
-	public Editora editora() {
-		return editora;
-	}
-	
-	public String getDataPublicacao() {
-		return dataPublicacao;
-	}
+    @ManyToManyField(model="Autor", references="autores")
+    private QuerySet<Autor> autores;
 
-	public String dataPublicacao() {
-		return dataPublicacao;
-	}
+    @ForeignKeyField(model="Editora", constraint_name="fk_livros_editoras", references="editoras")
+    private Editora editora;
 
-//	Setters
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public void titulo(String titulo) {
-		this.titulo = titulo;
-	}
+    @CharField(max_length=15, required=true, comment="Data de publicação do livro.")
+    private String dataPublicacao;
 
-	public void setAutores(List<Autor> autores) {
-		this.autores = new QuerySet<Autor>();
-		this.autores.add(autores);
-	}
-	
-	public void autores(List<Autor> autores) {
-		this.autores = new QuerySet<Autor>();
-		this.autores.add(autores);
-	}
-	
-	// Alternativa mais flexível.
-	public void setAutores(QuerySet autores) {
-		this.autores = autores;
-	}
-	
-	public void autores(QuerySet autores) {
-		this.autores = autores;
-	}
+    public static Manager objects = new Manager(Livro.class);
 
-	public void setEditora(Model editora) {
-		this.editora = (Editora) editora;
-	}
-	
-	public void editora(Model editora) {
-		this.editora = (Editora) editora;
-	}
-	
-	public void setDataPublicacao(String dataPublicacao) {
-		this.dataPublicacao = dataPublicacao;
-	}
-	
-	public void dataPublicacao(String dataPublicacao) {
-		this.dataPublicacao = dataPublicacao;
-	}
+    // Constructors
+    public Livro() {}
+
+    public Livro(int id) {
+        this.id = id;
+    }
+
+    public Livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Livro(String titulo, List<Autor> autores, Model editora, String dataPublicacao) {
+        this.titulo = titulo;
+        this.autores = new QuerySet<Autor>();
+        this.autores.add(autores);
+        this.editora = (Editora) editora;
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public Livro(int id, String titulo, List<Autor> autores, Model editora, String dataPublicacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autores = new QuerySet<Autor>();
+        this.autores.add(autores);
+        this.editora = (Editora) editora;
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    // Getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public QuerySet getAutores() {
+        return autores;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public String getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    // Setters
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = new QuerySet<Autor>();
+        this.autores.add(autores);
+    }
+
+    // More flexible alternative.
+    public void setAutores(QuerySet autores) {
+        this.autores = autores;
+    }
+
+    public void setEditora(Model editora) {
+        this.editora = (Editora) editora;
+    }
+
+    public void setDataPublicacao(String dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
 }

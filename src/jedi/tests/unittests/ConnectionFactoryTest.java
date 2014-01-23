af9b1477fa95c7baa6f30067cf8d17906908e557
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 12:48:01 -0200

    Update applied to ConnectionFactoryTest.java

--- src/jedi/tests/unittests/ConnectionFactoryTest.java
+++ src/jedi/tests/unittests/ConnectionFactoryTest.java
@@ -1,13 +1,20 @@
-/*******************************************************************************
+/***********************************************************************************************
+ * @(#)ConnectionFactoryTest.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
  * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     Thiago Alexandre Martins Monteiro - initial API and implementation
- ******************************************************************************/
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package jedi.tests.unittests;
 
 import java.sql.Connection;
@@ -19,24 +26,16 @@ import org.junit.Test;
 
 public class ConnectionFactoryTest {
 
-	@Test
-	public void testConnectH2() {
-		
-		// Obtendo conexão com o banco de dados H2.
-		Connection conexao = ConnectionFactory.connect("engine=h2");
-		
-		Assert.assertNotNull(conexao);
-	}
-	
-	@Test
-	public void testConnectMySQL() {
-		
-		// Obtendo conexão com o banco de dados H2.
-		Connection conexao = ConnectionFactory.connect("engine=mysql", "user=jedi", "password=jedi", "database=jedi");
-		// Definir "upd=jedi" como sendo um atalho para "user=jedi", "password=jedi", "database=jedi" 
-		//Connection conexao = ConnectionFactory.connect("engine=mysql", "upd=jedi");
-		
-		Assert.assertNotNull(conexao);
-	}
-	
+    @Test
+    public void testConnectH2() {
+        Connection conexao = ConnectionFactory.connect("engine=h2");
+        Assert.assertNotNull(conexao);
+    }
+
+    @Test
+    public void testConnectMySQL() {
+        Connection conexao = ConnectionFactory.connect("engine=mysql", "user=jedi", "password=jedi", "database=jedi");
+        Assert.assertNotNull(conexao);
+    }
+
 }

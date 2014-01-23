ecbb6deb9636be6679eac81c4f75156d31773c2a
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 11:52:24 -0200

    Update applied to Editora.java and EditoraTest.java

--- src/app/models/Editora.java
+++ src/app/models/Editora.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)Autor.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package app.models;
 
 import jedi.db.Models;
@@ -7,101 +24,77 @@ import jedi.db.models.Manager;
 import jedi.db.models.Model;
 
 public class Editora extends Model {
-//	Atributos
-	private static final long serialVersionUID = -4632483608458698726L;
+    // Attributes
+    private static final long serialVersionUID = -4632483608458698726L;
 
-	@CharField(max_length=30)
-	private String nome;
-	
-	@CharField(max_length=50, required=false)
-	private String endereco;
-	
-	@ForeignKeyField(model="Uf", constraint_name="fk_editoras_ufs", references="ufs", on_delete=Models.CASCADE)
-	private Uf uf;
-	
-	public static Manager objects = new Manager(Editora.class);
+    @CharField(max_length=30)
+    private String nome;
 
-//	Construtores
-	public Editora() {}
-	
-	public Editora(int id) {
-		this.id = id;
-	}
-	
-	public Editora(String nome) {
-		this.nome = nome;
-	}
-	
-	public Editora(String nome, Model uf) {
-		this.nome = nome;		
-		this.uf = (Uf) uf;
-	}
-	
-	public Editora(String nome, String endereco, Model uf) {
-		this.nome = nome;
-		this.endereco = endereco;
-		this.uf = (Uf) uf;
-	}
-	
-	public Editora(int id, String nome, String endereco, Model uf) {
-		this.id = id;
-		this.nome = nome;
-		this.endereco = endereco;
-		this.uf = (Uf) uf;
-	}
-	
-//	Getters
-	public String getNome() {
-		return nome;
-	}
+    @CharField(max_length=50, required=false)
+    private String endereco;
 
-	public String nome() {
-		return nome;
-	}
-	
-	public String getEndereco() {
-		return endereco;
-	}
-	
-	public String endereco() {
-		return endereco;
-	}
-	
-	public Uf getUf() {
-		return uf;
-	}
+    @ForeignKeyField(model="Uf", constraint_name="fk_editoras_ufs", references="ufs", on_delete=Models.CASCADE)
+    private Uf uf;
 
-	public Uf uf() {
-		return uf;
-	}
-	
-//	Setters
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-		
-	public void nome(String nome) {
-		this.nome = nome;
-	}
-	
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	
-	public void endereco(String endereco) {
-		this.endereco = endereco;
-	}
-	
-	public void setUf(Model uf) {
-		this.uf = (Uf) uf;
-	}
-	
-	public void uf(Model uf) {
-		this.uf = (Uf) uf;
-	}
+    public static Manager objects = new Manager(Editora.class);
 
-	@SuppressWarnings("rawtypes")
-	public jedi.db.models.QuerySet livro_set() {
-		return Livro.objects.get_set(Editora.class, this.id);
-	}
+    // Constructors
+    public Editora() {}
+
+    public Editora(int id) {
+        this.id = id;
+    }
+
+    public Editora(String nome) {
+        this.nome = nome;
+    }
+
+    public Editora(String nome, Model uf) {
+        this.nome = nome;
+        this.uf = (Uf) uf;
+    }
+
+    public Editora(String nome, String endereco, Model uf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.uf = (Uf) uf;
+    }
+
+    public Editora(int id, String nome, String endereco, Model uf) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.uf = (Uf) uf;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Uf getUf() {
+        return uf;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setUf(Model uf) {
+        this.uf = (Uf) uf;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public jedi.db.models.QuerySet livro_set() {
+        return Livro.objects.get_set(Editora.class, this.id);
+    }
 }

a5d2878a0e174081d2f3b354eb162c8f03ca31af
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 10:56:07 -0200

    Update applied to AutorTest.java

--- src/jedi/tests/unittests/AutorTest.java
+++ src/jedi/tests/unittests/AutorTest.java
@@ -34,48 +34,64 @@ public class AutorTest {
 
     @Test
     public void testInsert() {
+        
         Autor autorEsperado = new Autor();
         autorEsperado.setNome("Paulo");
         autorEsperado.setSobrenome("Coelhoo");
         autorEsperado.setEmail("paulocoelho@gmail.com");
         autorEsperado.save();
+        
         Autor autorObtido = Autor.objects.get("email", "paulocoelho@gmail.com");
+        
         Assert.assertEquals(autorEsperado.getId(), autorObtido.getId() );
     }
 
     @Test
     public void testUpdate() {
+        
         Autor autorEsperado = Autor.objects.get("email", "paulocoelho@gmail.com");
         autorEsperado.update("sobrenome='Coelho'");
+        
         Autor autorObtido = Autor.objects.get("email", "paulocoelho@gmail.com");
+        
         Assert.assertTrue(autorEsperado.getSobrenome().equals(autorObtido.getSobrenome() ) );
     }
 
     @Test
     public void testDelete() {
+        
         int esperado = 0;
+        
         Autor.objects.all().delete();
+        
         int obtido = Autor.objects.all().count();
+        
         Assert.assertEquals(esperado, obtido);
     }
 
     @Test
     public void testSaveInsert() {
+        
         Autor autorEsperado = new Autor();
         autorEsperado.setNome("John Ronald");
         autorEsperado.setSobrenome("Reuel Tolkienn");
         autorEsperado.setEmail("jrrtolkien@gmail.com");
         autorEsperado.save();
+        
         Autor autorObtido = Autor.objects.get("email", "jrrtolkien@gmail.com");
+        
         Assert.assertEquals(autorEsperado.getId(), autorObtido.getId() );
     }
 
     @Test
     public void testSaveUpdate() {
+        
         Autor autorEsperado = Autor.objects.get("email", "jrrtolkien@gmail.com");
         autorEsperado.setSobrenome("Reuel Tolkien");
         autorEsperado.save();
+        
         Autor autorObtido = Autor.objects.get("email", "jrrtolkien@gmail.com");
+        
         Assert.assertTrue(autorEsperado.getSobrenome().equals(autorObtido.getSobrenome() ) );
     }
 }

7ebee56b0242d9620384e3c91129b355d451e330
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 16:13:17 -0200

    Update applied to NullBooleanField.java

--- src/jedi/db/annotations/fields/NullBooleanField.java
+++ src/jedi/db/annotations/fields/NullBooleanField.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)NullBooleanField.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package jedi.db.annotations.fields;
 
 import java.lang.annotation.ElementType;
@@ -8,7 +25,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NullBooleanField {
-    public boolean required() default true; 
+    public boolean required() default true;
     public boolean unique() default false;
     public String default_value() default "null";
     public String comment() default "";

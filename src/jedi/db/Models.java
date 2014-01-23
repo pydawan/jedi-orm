819ee4575f1b064454a77ae51d691f8c905d46be
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 15:19:51 -0200

    Update applied to Models.java

--- src/jedi/db/Models.java
+++ src/jedi/db/Models.java
@@ -17,24 +17,25 @@
 
 package jedi.db;
 
-/** 
+/**
  * @author Thiago Alexandre Martins Monteiro
- *
+ * @version 1.0
+ * 
  */
 public enum Models {
-	CASCADE("CASCADE"),
-	PROTECT("PROTECT"),
-	SET_NULL("SET NULL"),
-	SET_DEFAULT("SET DEFAULT"),
-	DO_NOTHING("");
-	
-	private final String valor;
-	
-	private Models(String valor) {
-		this.valor = valor;
-	}
-	
-	public String getValor() {
-		return valor;
-	}
+    CASCADE("CASCADE"),
+    PROTECT("PROTECT"),
+    SET_NULL("SET NULL"),
+    SET_DEFAULT("SET DEFAULT"),
+    DO_NOTHING("");
+
+    private final String valor;
+
+    private Models(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
 }

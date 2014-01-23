f55490681735cc373a80ebf8365a64434580fb48
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 12:33:10 -0200

    Update applied to Pessoa.java

--- src/app/models/Pessoa.java
+++ src/app/models/Pessoa.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)Pessoa.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package app.models;
 
 import java.util.Date;
@@ -9,162 +26,112 @@ import jedi.db.models.Manager;
 import jedi.db.models.Model;
 
 public class Pessoa extends Model {
-//	Atributos
-	private static final long serialVersionUID = 887615433470716542L;
+    // Attributes
+    private static final long serialVersionUID = 887615433470716542L;
 
-	@CharField(max_length=50, unique=true, comment="Esse campo armazena o nome da pessoa.")
-	private String nome;
-	
-	@IntegerField
-	private int idade;
-	
-	@CharField(max_length=14, required=false)
-	private String cpf;
-	
-	@CharField(max_length=10, required=false)
-	private String rg;
-	
-	@CharField(max_length=1)
-	private String tipoPessoa;
-	
-//	Se auto_now for true criar campo no banco como sendo do tipo timestamp without timezone (with timezone não é uma boa estratégia).
-	@DateField(auto_now=true, auto_now_add=true, comment="Data do aniversário")
-	private Date dataNascimento;
-	
-//	Entity Manager dessa classe de objetos.
-	public static Manager objects = new Manager(Pessoa.class);
-	
-//	Construtores
-	public Pessoa() {}
-	
-	public Pessoa(int id, String nome, int idade, String tipoPessoa) {
-		this.id = id;
-		this.nome = nome;
-		this.idade = idade;		
-		this.tipoPessoa = tipoPessoa;		
-	}
-	
-	public Pessoa(String nome, int idade, String tipoPessoa) {		
-		this.nome = nome;
-		this.idade = idade;		
-		this.tipoPessoa = tipoPessoa;		
-	}	
-		
-	public Pessoa(int id, String nome, int idade, String cpf, String rg, String tipoPessoa, Date dataNascimento) {
-		this.id = id;
-		this.nome = nome;
-		this.idade = idade;
-		this.cpf = cpf;
-		this.rg = rg;
-		this.tipoPessoa = tipoPessoa;
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public Pessoa(String nome, int idade, String cpf, String rg, String tipoPessoa, Date dataNascimento) {
-		this.nome = nome;
-		this.idade = idade;
-		this.cpf = cpf;
-		this.rg = rg;
-		this.tipoPessoa = tipoPessoa;
-		this.dataNascimento = dataNascimento;
-	}
+    @CharField(max_length=50, unique=true, comment="Esse campo armazena o nome da pessoa.")
+    private String nome;
 
-	
-//	Getters
-	public String getNome() {
-		return nome;
-	}
-	
-	public String nome() {
-		return nome;
-	}
-	
-	public int getIdade() {
-		return idade;
-	}
-	
-	public int idade() {
-		return idade;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	
-	public String cpf() {
-		return cpf;
-	}
-	
-	public String getRg() {
-		return rg;
-	}
-	
-	public String rg() {
-		return rg;
-	}
-	
-	public String getTipoPessoa() {
-		return tipoPessoa;
-	}
-	
-	public String tipo_pessoa() {
-		return tipoPessoa;
-	}
-	
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-	
-	public Date data_nascimento() {
-		return dataNascimento;
-	}
+    @IntegerField
+    private int idade;
 
-//	Setters
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public void nome(String nome) {
-		this.nome = nome;
-	}
+    @CharField(max_length=14, required=false)
+    private String cpf;
 
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	
-	public void idade(int idade) {
-		this.idade = idade;
-	}
-	
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public void cpf(String cpf) {
-		this.cpf = cpf;
-	}
+    @CharField(max_length=10, required=false)
+    private String rg;
 
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	
-	public void rg(String rg) {
-		this.rg = rg;
-	}
+    @CharField(max_length=1)
+    private String tipoPessoa;
 
-	public void setTipoPessoa(String tipoPessoa) {
-		this.tipoPessoa = tipoPessoa;
-	}
-	
-	public void tipo_pessoa(String tipo_pessoa) {
-		this.tipoPessoa = tipo_pessoa;
-	}
-	
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public void data_nascimento(Date data_nascimento) {
-		this.dataNascimento = data_nascimento;
-	}
+    @DateField(auto_now=true, auto_now_add=true, comment="Data do aniversário")
+    private Date dataNascimento;
+
+    public static Manager objects = new Manager(Pessoa.class);
+
+    // Constructors
+    public Pessoa() {}
+
+    public Pessoa(int id, String nome, int idade, String tipoPessoa) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public Pessoa(String nome, int idade, String tipoPessoa) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public Pessoa(int id, String nome, int idade, String cpf, String rg, String tipoPessoa, Date dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.tipoPessoa = tipoPessoa;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Pessoa(String nome, int idade, String cpf, String rg, String tipoPessoa, Date dataNascimento) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.tipoPessoa = tipoPessoa;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public String getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public void setTipoPessoa(String tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
 }

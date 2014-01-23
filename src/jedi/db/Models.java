19132f22b938a01f36049cec284849a9317b83d0
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 15:12:42 -0200

    Update applied to Models.java

--- src/jedi/db/Models.java
+++ src/jedi/db/Models.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)Models.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package jedi.db;
 
 /** 
@@ -18,10 +35,6 @@ public enum Models {
 	}
 	
 	public String getValor() {
-		return valor;
-	}
-	
-	public String valor() {
 		return valor;
 	}
 }

cc98457802f2f5d2d9ed32ca79995d61cd3ac9b4
Thiago Monteiro <pydawan@gmail.com>
2014-01-23 15:59:36 -0200

    Update applied to ForeignKeyField.java

--- src/jedi/db/annotations/fields/ForeignKeyField.java
+++ src/jedi/db/annotations/fields/ForeignKeyField.java
@@ -1,3 +1,20 @@
+/***********************************************************************************************
+ * @(#)ForeignKeyField.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/23
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
 package jedi.db.annotations.fields;
 
 import java.lang.annotation.ElementType;
@@ -10,20 +27,12 @@ import jedi.db.Models;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ForeignKeyField {
-
-	public String model();
-	
-	public String column_name() default "";
-	
-	public String constraint_name();
-	
-	public String references();
-	
-	public String referenced_column() default "";
-	
-	public String comment() default "";
-	
-	public Models on_delete() default Models.CASCADE;
-	
-	public Models on_update() default Models.CASCADE;
+    public String model();
+    public String column_name() default "";
+    public String constraint_name();
+    public String references();
+    public String referenced_column() default "";
+    public String comment() default "";
+    public Models on_delete() default Models.CASCADE;
+    public Models on_update() default Models.CASCADE;
 }

bf4077fa1e68ab66825d7609285ee5a76c4a122e
Thiago Monteiro <pydawan@gmail.com>
2014-01-30 13:17:42 -0200

    Added BookTest.java to the project.

---
+++ src/jedi/tests/unittests/BookTest.java
@@ -0,0 +1,46 @@
+/***********************************************************************************************
+ * @(#)BookTest.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: 2014/01/30
+ * 
+ * Copyright (c) 2014 Thiago Alexandre Martins Monteiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the GNU Public License v2.0 which accompanies 
+ * this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *    Thiago Alexandre Martins Monteiro - initial API and implementation
+ ************************************************************************************************/
+
+package jedi.tests.unittests;
+
+import jedi.db.models.QuerySet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import app.models.Author;
+import app.models.Book;
+import app.models.Country;
+import app.models.Publisher;
+import app.models.State;
+
+public class BookTest {
+
+    @Test
+    public void testInsert() {
+        Book expectedBook = new Book();
+        expectedBook.setTitle("O Alquimista");
+        expectedBook.setPublicationDate("17/10/2013");
+        expectedBook.setPublisher(new Publisher("Editora Abril", new State("São Paulo", "SP", new Country("Brasil", "BR"))));
+        QuerySet<Author> authors = new QuerySet<Author>();
+        authors.add(new Author("Paulo", "Coelho", "paulocoelho@gmail.com"));
+        expectedBook.setAuthors(authors);
+        expectedBook.save();
+        Book obtainedBook = Book.objects.get("title", "O Alquimista");
+        Assert.assertEquals(expectedBook.getId(), obtainedBook.getId());
+    }
+}

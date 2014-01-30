4167e9f8596a7dfe5b30411a54dc1a12be31b627
Thiago Monteiro <pydawan@gmail.com>
2014-01-30 19:42:22 -0200

    Added a explanatory code in cleanUp()

--- src/jedi/tests/unittests/BookTest.java
+++ src/jedi/tests/unittests/BookTest.java
@@ -19,6 +19,7 @@ package jedi.tests.unittests;
 
 import jedi.db.models.QuerySet;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,17 +31,37 @@ import app.models.State;
 
 public class BookTest {
 
+    @AfterClass
+    public static void cleanUp() {
+        /*
+        for (Book book : Book.objects.<Book>all()) {
+            book.delete();
+        }*/
+        
+        // Alternative form.
+        Book.objects.all().delete();
+    }
+    
     @Test
     public void testInsert() {
         Book expectedBook = new Book();
-        expectedBook.setTitle("O Alquimista");
+        expectedBook.setTitle("O Alquimistaa");
         expectedBook.setPublicationDate("17/10/2013");
         expectedBook.setPublisher(new Publisher("Editora Abril", new State("São Paulo", "SP", new Country("Brasil", "BR"))));
         QuerySet<Author> authors = new QuerySet<Author>();
         authors.add(new Author("Paulo", "Coelho", "paulocoelho@gmail.com"));
         expectedBook.setAuthors(authors);
         expectedBook.save();
-        Book obtainedBook = Book.objects.get("title", "O Alquimista");
+        Book obtainedBook = Book.objects.get("title", "O Alquimistaa");
         Assert.assertEquals(expectedBook.getId(), obtainedBook.getId());
+    }
+    
+    @Test
+    public void testUpdate() {
+        Book expectedBook = new Book();
+        expectedBook.setTitle("O Alquimista");
+        Book obtainedBook = Book.objects.get("title", "O Alquimistaa");
+        obtainedBook.update("title='O Alquimista'");
+        Assert.assertEquals(expectedBook.getTitle(), obtainedBook.getTitle());
     }
 }
